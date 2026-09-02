979f46b75b9161818c3d690cec507dc47204e802
Yetso <dav.catoul@gmail.com>
2026-09-02 13:30:26 +0200

    fix: add non-null verification to client

--- lua/nvim-navic/init.lua
+++ lua/nvim-navic/init.lua
@@ -76,7 +76,7 @@ local function setup_auto_attach(opts)
 	vim.api.nvim_create_autocmd("LspAttach", {
 		callback = function(args)
 			local client = vim.lsp.get_client_by_id(args.data.client_id)
-			if not client.server_capabilities.documentSymbolProvider then
+			if not client or not client.server_capabilities.documentSymbolProvider then
 				return
 			end
 
